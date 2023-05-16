287350576bd4fd9b5d7ca3d1d5f448a28f016bb4
Shizik55 <amyrsabekova@mail.ru>
2023-05-16 12:19:43 +0600

    second commit

--- intro.py
+++ intro.py
@@ -4,3 +4,19 @@
 # GitHub - это веб-сайт для размещения git-репозиториев в совместной разработке проектов.
 
 # Репозиторий - это хранилище нашего кода и истории его изменений.
+
+
+# git init                                          (добавляет невидимый .git; создаёт удалённыйй репозиторий, в той директории, в которой юыла прописана команда)
+# git add git_lesson/   git add .                   (говорит гиту читать только данный блок(директорий или файл); команда для добавления изменений в систему отслеживания гита)
+# git config --global user.email "you@example.com"  (указать почту пользователя устройства)
+# git config --global user.name "Your Name"         (указать имя пользователя устройства)
+# git commit -m "first commit"                      (комментирование изменения; обязательно)
+# git branch                                        (провереят ветки)
+# git branch -M mian                                (у каждого ползователя должна быть своя ветка; все ветки сходятся в master (или main в mac))
+# git remote add origin https://github.com/Shizik5/ExampleFirst.git
+# git push -u origin main
+
+
+
+# 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  -   Example_token из https://github.com/Shizik5/ExampleFirst.git
+
